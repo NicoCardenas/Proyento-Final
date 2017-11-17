9b052b93178e3fc80263e2b6e850ecbfdc7f13dd
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2017-11-16 23:14:46 -0500

    prueba beta de la interface
    version fea de la interface grafica

--- POOAgeOfWar/src/Presentacion/AgeOfWar.java
+++ POOAgeOfWar/src/Presentacion/AgeOfWar.java
@@ -1,16 +1,43 @@
 package Presentacion;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
 public class AgeOfWar extends JFrame{
 
+	static JFrame ventanaPadre = new MenuGUI();
+
 	public AgeOfWar() {
-		// TODO Auto-generated constructor stub
+		super();
+		prepararElementos();
+		prepareAcciones();
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private void prepararElementos() {
+		//Configuracion del la ventana
+		setTitle("Age Of War");
+		setSize(500, 500);
+		setResizable(false);
+		center();
 	}
 
+	private void prepareAcciones() {
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				dispose();
+				ventanaPadre.setVisible(true);
+			}
+		});
+	}
+	
+	private void center(){
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (screen.width - getSize().width)/2;
+		int y = (screen.height - getSize().height)/2;
+		setLocation(x,y);
+	}
 }
--- POOAgeOfWar/src/Presentacion/MenuGUI.java
+++ POOAgeOfWar/src/Presentacion/MenuGUI.java
@@ -7,11 +7,18 @@ import javax.swing.*;
 
 public class MenuGUI extends JFrame {
 	
+	//Elementos de la ventana
+	public static MenuGUI ventana = new MenuGUI();
 	JButton jugar = new JButton("Jugar");
+	JButton reglas = new JButton("Reglas");
 	JButton creditos = new JButton("Creditos");
 	Container contenedor = new Container();
-	JPanel principal = new JPanel(new GridLayout( 3, 10));
-	JLabel titulo = new JLabel("Titulo", JLabel.CENTER);
+	JPanel principal = new JPanel(new GridLayout( 4, 1));
+	JLabel titulo = new JLabel("POO Age of war", JLabel.CENTER);
+	//Ventanas adicionales
+	AgeOfWar vjuego = new AgeOfWar();
+	Reglas vreglas = new Reglas();
+	Creditos vcreditos = new Creditos();
 
 	public MenuGUI() {
 		super();
@@ -20,13 +27,6 @@ public class MenuGUI extends JFrame {
 	}
 
 	public static void main(String[] args) {
-		MenuGUI ventana = new MenuGUI();
-		ventana.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent evt) {
-				exitAccion();
-				}
-			}
-		);
 		ventana.setVisible(true);
 	}
 	
@@ -36,16 +36,50 @@ public class MenuGUI extends JFrame {
 		setSize(500, 500);
 		setResizable(false);
 		center();
+		principal.setBackground(Color.WHITE);
+		jugar.setLayout(new BorderLayout(10, 10));
 		titulo.setFont(new Font("Arial", 0, 32));
 		// Agregar elementos
 		principal.add(titulo);
 		principal.add(jugar);
+		principal.add(reglas);
 		principal.add(creditos);
 		add(principal);
 	}
 	
 	private void prepareAcciones(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				exitAccion();
+				}
+			}
+		);
+		
+		jugar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				vjuego.setVisible(true);
+				ventana.dispose();
+			}
+		});
+		
+		reglas.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				vreglas.setVisible(true);
+				ventana.dispose();
+			}
+		});
+		
+		creditos.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				vcreditos.setVisible(true);
+				ventana.dispose();
+			}
+		});
 	}
 	
 	private void center(){
@@ -55,7 +89,7 @@ public class MenuGUI extends JFrame {
 		setLocation(x,y);
 	}
 
-	private static void exitAccion(){
+	public static void exitAccion(){
 		if (JOptionPane.showConfirmDialog(null,"Esta seguro que desea salir? ", "Salir del sistema",JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
 			System.exit(0);
 	}
